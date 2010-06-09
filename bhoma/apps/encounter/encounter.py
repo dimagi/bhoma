c6e385a9d02c498515cc2ab9a1ab3da902c367ed
Cory Zue <czue@dimagi.com>
2010-06-09 18:04:12 -0400

    removing copy/paste mess from file

--- bhoma/apps/encounter/encounter.py
+++ bhoma/apps/encounter/encounter.py
@@ -19,25 +19,3 @@ class EncounterTypeBase(object):
         pass
     
 
-from couchdbkit.ext.django.schema import *
-from bhoma.apps.patient.models import CPatient
-
-"""
-Couch models go here.  
-"""
-
-class Encounter(Document):
-    created = DateTimeProperty(required=True)
-    edited = DateProperty(required=True)
-    type = StringProperty()
-    patient = SchemaProperty(CPatient())
-    is_deprecated = BooleanProperty()
-    previous_encounter_id = StringProperty()
-    
-    class Meta:
-        app_label = 'encounter'
-
-    def __unicode__(self):
-        return "%s (%s)" % (self.type, self.created)
-    
-        
