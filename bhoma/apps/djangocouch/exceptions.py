d89ea893cb21d890ed0ec3a09364aa0bd31d76e4
Cory Zue <czue@dimagi.com>
2010-06-24 17:27:11 -0400

    fix bad exception reference

--- bhoma/apps/djangocouch/exceptions.py
+++ bhoma/apps/djangocouch/exceptions.py
@@ -1,6 +1,6 @@
 
 
-def ModelPreconditionNotMet(Exception):
+class ModelPreconditionNotMet(Exception):
     """
     A custom exception indicating that a model precondition was
     not met in trying to save something to couch.
