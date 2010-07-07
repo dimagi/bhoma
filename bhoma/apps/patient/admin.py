0754488968c96721a7c80cd0e61ef8fc6e6ebd70
Cory Zue <czue@dimagi.com>
2010-07-07 17:31:55 -0400

    remove auxiliary, unused, deprecated django models from patient app

--- bhoma/apps/patient/admin.py
+++ bhoma/apps/patient/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from bhoma.apps.patient.models import *
 
-admin.site.register(Address)
-admin.site.register(Person)
-admin.site.register(Patient)
+# admin.site.register(Address)
--- bhoma/apps/patient/models/__init__.py
+++ bhoma/apps/patient/models/__init__.py
@@ -1,2 +1,1 @@
-from bhoma.apps.patient.models.django import PersonType, Person, Patient, Phone, Address
 from bhoma.apps.patient.models.couch import CClinic, CDistrict, CPatient
