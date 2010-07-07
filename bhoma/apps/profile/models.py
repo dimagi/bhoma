84c5d1058b9b2cff2b360240112973457733c01f
Cory Zue <czue@dimagi.com>
2010-07-07 17:32:45 -0400

    add chw_id to the profile object to link django logins to chws.

--- bhoma/apps/profile/models.py
+++ bhoma/apps/profile/models.py
@@ -7,7 +7,11 @@ class BhomaUserProfile(CouchUserProfile):
     The Bhoma Profile object, which saves the user data in couch along
     with annotating the additional fields we need for BHOMA
     """
-    clinic_id = models.CharField(max_length=100, default=settings.BHOMA_CLINIC_ID)
+    clinic_id = models.CharField(max_length=100, default=settings.BHOMA_CLINIC_ID,
+                                 help_text="The clinic this user works out of")
+    
+    chw_id = models.CharField(max_length=100, blank=True,
+                              help_text="The couch id of the chw object, if it exists")
     
 # load our signals.
 import bhoma.apps.profile.signals 
