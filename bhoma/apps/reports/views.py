4cc526a2f16d871fcaf9039c77c0d0db23b6061e
Cory Zue <czue@dimagi.com>
2010-07-07 17:31:17 -0400

    add reports app, which has a single report + couch view for closed but unrecorded cases.

---
+++ bhoma/apps/reports/views.py
@@ -0,0 +1,15 @@
+from bhoma.apps.case.models import CReferral
+from bhoma.utils import render_to_response
+
+def unrecorded_referral_list(request):
+    """
+    Clinic able to pull up list of Open Cases that require bookkeeping.
+    Any open case without a follow-up either at the clinic or from the 
+    CHW after 6 weeks is given the Outcome: 'Lost Follow Up.' The report 
+    also lists cases closed by the CHW and their outcome for the CSW to 
+    record in the patient folder.
+    """
+    # display list of open cases but unrecorded cases (referrals)
+    referrals = CReferral.view("reports/closed_unrecorded_referrals")
+    return render_to_response(request, "reports/closed_unrecorded_referrals.html",
+                              {"referrals": referrals})
