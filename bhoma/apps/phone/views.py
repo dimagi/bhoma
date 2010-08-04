7429f4793397a3fa6b1dd1149236e81db5c2c616
Drew Roos <droos@dimagi.com>
2010-08-04 16:14:40 -0400

    update phone/test/ test data to have bhoma case info

--- bhoma/apps/phone/views.py
+++ bhoma/apps/phone/views.py
@@ -65,42 +65,52 @@ def test(request):
 
 TESTING_RESTORE_DATA=\
 """<restoredata> 
-<n0:registration xmlns:n0="http://openrosa.org/user-registration"><username>bhoma</username><password>234</password><uuid>O9KNJQO8V951GSOXDV7604I1Q</uuid><date>2010-07-28</date><registering_phone_id>SSNCFBLR8U12WB3I8RMKRTACC</registering_phone_id><user_data><data key="providertype">hbcp</data><data key="training">yes</data><data key="sex">m</data><data key="user_type">standard</data></user_data></n0:registration>
+<n0:registration xmlns:n0="http://openrosa.org/user-registration">
+  <username>bhoma</username>
+  <password>234</password>
+  <uuid>O9KNJQO8V951GSOXDV7604I1Q</uuid>
+  <date>2010-07-28</date>
+  <registering_phone_id>SSNCFBLR8U12WB3I8RMKRTACC</registering_phone_id>
+  <user_data>
+    <data key="providertype">hbcp</data>
+    <data key="training">yes</data>
+    <data key="sex">m</data>
+    <data key="user_type">standard</data>
+  </user_data>
+</n0:registration>
+
 <case>
-    <case_id> 
-        PZHBCC9647XX0V4YAZ2UUPQ9M
-    </case_id> 
-    <date_modified> 
-        2010-07-28T14:49:57.930
-    </date_modified> 
-    <create> 
-        <case_type_id> 
-            cc_pf_client
-        </case_type_id> 
-        <user_id> 
-            O9KNJQO8V951GSOXDV7604I1Q
-        </user_id> 
-        <case_name> 
-            PM-SONGEA
-        </case_name> 
-        <external_id> 
-            .
-        </external_id> 
-    </create> 
-    <update> 
-        <pat_inits> 
-            PM
-        </pat_inits> 
-        <sex> 
-            m
-        </sex> 
-        <dob> 
-            1970-07-28
-        </dob> 
-        <village> 
-            SONGEA
-        </village> 
-    </update> 
-</case> 
+<case_id>PZHBCC9647XX0V4YAZ2UUPQ9M</case_id>
+<date_modified>2010-07-28T14:49:57.930</date_modified>
+<create>
+  <case_type_id>bhoma_followup</case_type_id>
+  <user_id>O9KNJQO8V951GSOXDV7604I1Q</user_id>
+  <case_name>6</case_name>
+  <external_id>bhoma8972837492818239</external_id>
+</create>
+<update>
+  <first_name>DREW</first_name>
+  <last_name>ROOS</last_name>
+  <birth_date>1983-10-06</birth_date>
+  <birth_date_est>1</birth_date_est>
+  <age>26</age>
+  <sex>m</sex>
+  <village>SOMERVILLE</village>
+  <contact>9183739767</contact>
+
+  <followup_type>missed_appt</followup_type>
+  <orig_visit_type>general</orig_visit_type>
+  <orig_visit_diagnosis>malaria</orig_visit_diagnosis>
+  <orig_visit_date>2010-07-12</orig_visit_date>
+
+  <activation_date>2010-07-27</activation_date>
+  <due_date>2010-07-31</due_date>
+
+  <missed_appt_date>2010-07-24</missed_appt_date>
+  <ttl_missed_appts>1</ttl_missed_appts>
+</update>
+</case>
+
+
 </restoredata>
 """
