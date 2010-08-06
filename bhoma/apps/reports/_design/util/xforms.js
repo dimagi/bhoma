407fef225c8cda23551892dfa8b7dd66c8734c48
Cory Zue <czue@dimagi.com>
2010-08-06 20:31:30 +0200

    attempt to fix ubuntu date parsing issue

--- bhoma/apps/reports/_design/util/xforms.js
+++ bhoma/apps/reports/_design/util/xforms.js
@@ -18,9 +18,16 @@ function xform_matches(doc, namespace) {
 var exists = function(basestring, searchstring) {
     return basestring && basestring.indexOf(searchstring) >= 0;
 }
-        
-        
+
+// parse a date in yyyy-mm-dd format
+function parse_date(date_string) {
+    // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse    
+    var parts = date_string.match(/(\d+)/g);
+    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+    return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
+     
 
 function get_encounter_date(xform_doc) {
-    return new Date(Date.parse(xform_doc.encounter_date));
+    return parse_date(xform_doc.encounter_date);
 }
