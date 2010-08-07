78568d48e88e03523c71441966033759b15cf84b
Cory Zue <czue@dimagi.com>
2010-08-07 12:50:33 +0200

    fix bug in mixin where it stack overflowed if no unicode method was defined

--- bhoma/utils/mixins.py
+++ bhoma/utils/mixins.py
@@ -7,3 +7,7 @@ class UnicodeMixIn(object):
     def __str__(self):
         return unicode(self).encode('utf-8')
     
+    def __unicode__(self):
+        # avoid recursion when we forget to override this
+        return super(UnicodeMixIn, self).__str__()
+    
