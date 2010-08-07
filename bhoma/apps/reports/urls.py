f3ad9f100f4e08ef828792bc9bfb7a2a58dee898
Cory Zue <czue@dimagi.com>
2010-08-07 12:49:30 +0200

    changing report list url to /

--- bhoma/apps/reports/urls.py
+++ bhoma/apps/reports/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-                       url(r'^list/$', login_required(direct_to_template), 
+                       url(r'^$', login_required(direct_to_template), 
                            {"template": "reports/report_list.html"}, name="report_list"),
                        url(r'^unrecorded/$', 'bhoma.apps.reports.views.unrecorded_referral_list', 
                            name='unrecorded_referral_list'),
