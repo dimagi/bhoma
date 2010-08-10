3bd45e8f70c28aa78d3449376380ab25dd440f7a
Cory Zue <czue@dimagi.com>
2010-08-10 19:03:11 +0200

    adding continuous sync support (not sure it works)

--- bhoma/utils/couch/sync.py
+++ bhoma/utils/couch/sync.py
@@ -3,7 +3,7 @@ from couchdbkit.client import Server
 from bhoma import const
 
 
-def pull_from_national_to_clinic():
+def pull_from_national_to_clinic(continuous=False):
     """ 
     Pull data from a remote nationally-configured database to a local one
     """ 
@@ -16,23 +16,33 @@ def pull_from_national_to_clinic():
     target = settings.BHOMA_COUCH_DATABASE_NAME
     filter = const.FILTER_CLINIC
     query_params = { "clinic_id": settings.BHOMA_CLINIC_ID }
-    return replicate(server, source, target, filter, query_params)
+    return replicate(server, source, target, continuous, filter, query_params)
 
-def push_from_clinic_to_national():
+def push_from_clinic_to_national(continuous=False):
     """ 
     Push data from the local database to the nationally-configured one
     """ 
     server = Server(settings.BHOMA_COUCH_SERVER)
     source = settings.BHOMA_COUCH_DATABASE_NAME
     target = _get_national_db_from_settings()
-    return replicate(server, source, target)
+    return replicate(server, source, target, continuous)
 
 
-def replicate(server, source, target, filter="", query_params={}):
+def replicate(server, source, target, continuous=False, filter="", query_params={}):
     replication_params = {"source": source, 
                           "target": target,
+                          "continuous": continuous,
                           "filter": filter,
                           "query_params": query_params
+    }
+    result = server.res.post('/_replicate', payload=replication_params)
+    return result
+
+def cancel_replication(server, source, target):
+    replication_params = {"source": source, 
+                          "target": target,
+                          "continuous": True,
+                          "cancel": True
     }
     result = server.res.post('/_replicate', payload=replication_params)
     return result
