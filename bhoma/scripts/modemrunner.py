ca57f3db13b351d6ed01fdbcf9092774046f569e
root <root@mandombe-clinic-server.(none)>
2010-08-13 11:25:53 +0200

    initial hacky modemrunner script

--- bhoma/scripts/modemrunner.py
+++ bhoma/scripts/modemrunner.py
@@ -6,25 +6,62 @@ import time
 
 # TODO: google python script test if you are connected to the internet
 def wvdial_success(proc):
-    # give it some time to do its thing
-    time.sleep(5)
-    # hacky hacky, just check the output to see if we're happy
-    out = proc.stdout.read()
-    return "IP address" in stdout
+    # hack
+    # We assume that if it hasn't returned after this amount of time then
+    # it is happy
+    TIMEOUT = 20
+    wait_time = 0
+    INTERVAL_TIME = 5
+    while wait_time < TIMEOUT:
+        # if it returned, something bad happened
+        code = proc.poll()
+        if code:
+            print "ERROR: wvdial exited with code %s" % code
+            return False
+        # give it some time to do its thing
+        print "going to sleep for %s seconds, total wait time is %s seconds" % \
+            (INTERVAL_TIME, wait_time)
+        time.sleep(INTERVAL_TIME)
+        wait_time += INTERVAL_TIME
+
+    return True
+    # TODO: hacky hacky, just check the output to see if we're happy
+    # return "IP address" in out
     
 
 def test_public_connection():
     # hacky hacky
-    ping_proc = Popen("ping -c 3 google.com", stdout=PIPE, shell=True)
+    print "pinging google to test public connection."
+    ping_proc = Popen("ping -c 5 google.com", stdout=PIPE, stderr=PIPE, shell=True)
     ret = ping_proc.wait()
-    out = ping_proc.stdout.read()
-    # TODO: ping in python?
-    return not "0 received" in out
-    
-wvdial_proc = Popen("echo Hello World", stdout=PIPE, shell=True)
-if wvdial_success(wvdial_proc):
-    print "wvdial thinks it has a connection"
+    if ret == 0:
+        out = ping_proc.stdout.read()
+        # TODO: ping/test in native python?
+        return not "0 received" in out
+    print "ping failed! %s" % ping_proc.stderr.read()
+    return False
+
+def full_modem_config():
     if test_public_connection():
-        print "public connection successful!"
-    else:
-        print "failed to connect!"
+        print "already connected!"
+        return True
+    else: 
+        print "not connected starting wvdial"
+        wvdial_configs = ["defaults", "alt1", "alt2", "alt3"]
+        for conf in wvdial_configs:
+            command = "wvdial %s" % conf
+            print "running %s" % command
+            wvdial_proc = Popen(command, stdout=PIPE, shell=True)
+            if wvdial_success(wvdial_proc):
+                print "wvdial thinks it has a connection"
+                if test_public_connection():
+                    print "public connection successful!"
+                    return True
+                else:
+                    print "failed to connect!"
+
+        # down here we failed.
+        return False
+
+if __name__ == "__main__":
+    full_modem_config()
