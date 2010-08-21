230b8891db2a39734011e804fdaab08c3776ac19
Cory Zue <czue@dimagi.com>
2010-08-21 12:24:14 +0200

    move drug models to subfolder since we're adding couch models

--- bhoma/apps/drugs/models/django.py
+++ bhoma/apps/drugs/models/django.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-
+from __future__ import absolute_import
 import re
 from django.db import models
 from bhoma.apps.djangocouch.models import CouchModel
