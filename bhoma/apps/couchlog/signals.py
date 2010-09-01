fca8fc40fbfea81d60d0f5624dff36df71b15ba6
Cory Zue <czue@dimagi.com>
2010-09-01 17:46:44 -0400

    add clinic id to exception records

--- bhoma/apps/couchlog/signals.py
+++ bhoma/apps/couchlog/signals.py
@@ -1,8 +1,12 @@
 from django.core.signals import got_request_exception
+from django.conf import settings
 
 def handle_exception(sender, request, **kwargs):
     from bhoma.apps.couchlog.models import ExceptionRecord
     record = ExceptionRecord.from_request_exception(request)
+    # bolt on the clinic code too
+    record.clinic_id = settings.BHOMA_CLINIC_ID
+    record.save()
     
     
 
