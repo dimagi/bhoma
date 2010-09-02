1329e6fd7f800e81fce315862617b19e7aedca9c
Cory Zue <czue@dimagi.com>
2010-09-02 18:15:26 -0400

    add conflict resolution to the patient loader

--- bhoma/apps/patient/loader.py
+++ bhoma/apps/patient/loader.py
@@ -1,5 +1,8 @@
 from bhoma.apps.patient.models import CPatient
 from bhoma.const import VIEW_ALL_PATIENTS
+from bhoma.utils.couch.database import get_db
+from bhoma.utils import logging
+from bhoma.apps.patient.processing import reprocess
 
 
 def get_patient(patient_id):
@@ -8,5 +11,24 @@ def get_patient(patient_id):
     will run through all the xforms and regenerate the patient, deleting 
     all other revisions.
     """
-    patient = CPatient.view(VIEW_ALL_PATIENTS, key=patient_id).one()
-    return patient
+    
+    def resolve_conflicts(pat_id, conflicts):
+        # For now conflict resolution assumes that all you ever need 
+        # to do is reprocess the patient and delete the conflicts.
+        reprocess(pat_id)
+        conflict_docs = [get_db().get(pat_id, rev=conflict) for conflict in conflicts]
+        get_db().bulk_delete(conflict_docs)
+        
+    pat_data = get_db().get(patient_id, conflicts=True)
+    
+    if "_conflicts" in pat_data:
+        tries = 0
+        while tries < 5:
+            try:
+                resolve_conflicts(patient_id, pat_data["_conflicts"])
+                break
+            except Exception, e:
+                logging.log_exception(e)
+                tries = tries + 1
+     
+    return CPatient.get(patient_id)
