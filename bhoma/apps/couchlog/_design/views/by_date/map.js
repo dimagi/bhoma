06abb2f97d209dc59f6ef651b2a0ba1cd0f82158
Cory Zue <czue@dimagi.com>
2010-09-07 19:02:41 -0400

    fix clinic/date view of couchlogs

--- bhoma/apps/couchlog/_design/views/by_date/map.js
+++ bhoma/apps/couchlog/_design/views/by_date/map.js
@@ -1,6 +1,6 @@
 function(doc) { 
     if (doc.doc_type == "ExceptionRecord")
     {
-        emit(doc.clinic_id, new Date(doc.date), doc);
+        emit([doc.clinic_id, new Date(doc.date)], doc);
     }
 }
