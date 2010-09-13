8cf9512cdcf9f51da4949ac7a9ec0179c3993a38
Cory Zue <czue@dimagi.com>
2010-09-13 18:13:22 -0400

    better synclog admin

--- bhoma/apps/phone/admin.py
+++ bhoma/apps/phone/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from bhoma.apps.phone.models import SyncLog
 
-admin.site.register(SyncLog)
+class SyncAdmin(admin.ModelAdmin):
+    list_display = ("date", "operation", "phone_id", "chw_id", "last_seq")
+    list_filter = ("date", "operation", "phone_id", "chw_id")
+    
+admin.site.register(SyncLog, SyncAdmin)
