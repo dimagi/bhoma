59e460220d08eefce682714789e7e5397113f4d7
Cory Zue <czue@dimagi.com>
2010-09-14 16:25:38 -0400

    add user summary report

--- bhoma/apps/reports/_design/util/xforms.js
+++ bhoma/apps/reports/_design/util/xforms.js
@@ -6,7 +6,6 @@
 /*
  * use this to make sure you loaded the reports module properly
  */
-
 function hello_xforms() {
     log("hello xforms!");
 }
@@ -19,6 +18,15 @@ var exists = function(basestring, searchstring) {
     return basestring && basestring.indexOf(searchstring) >= 0;
 }
 
+function get_date_string(xform_doc) {
+	// check some expected places for a date
+	if (xform_doc.encounter_date) return xform_doc.encounter_date;
+	if (xform_doc.date) return xform_doc.date;
+    if (xform_doc.meta && xform_doc.meta.TimeEnd) return xform_doc.meta.TimeEnd;
+	if (xform_doc.meta && xform_doc.meta.TimeStart) return xform_doc.meta.TimeStart;
+	return null;
+}
+
 // parse a date in yyyy-mm-dd format
 function parse_date(date_string) {
     // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse    
@@ -26,8 +34,11 @@ function parse_date(date_string) {
     // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
     return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
 }
-     
 
 function get_encounter_date(xform_doc) {
-    return parse_date(xform_doc.encounter_date);
+    date_str = get_date_string(xform_doc);
+    if (date_str) {
+        return parse_date(date_str);
+    }
+    return null;
 }
