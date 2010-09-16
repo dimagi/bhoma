cd2b40bf1d4f999c2587cb6e0884a5319c0e61a4
Cory Zue <czue@dimagi.com>
2010-09-16 16:21:20 -0400

    better error handling - don't write a wacky post object when you get a regular text content type.

--- bhoma/apps/couchlog/models.py
+++ bhoma/apps/couchlog/models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import json
 import sys
 import traceback
+import urllib2
 
 class ExceptionRecord(Document):
     """
@@ -25,11 +26,16 @@ class ExceptionRecord(Document):
         that generated it)
         """
         url = request.build_absolute_uri()
+        use_raw_data = False
         if request.method == "GET":
             query_params = request.GET
         else:
-            query_params = request.POST
-        
+            if request.META["CONTENT_TYPE"].startswith('text'):
+                # if we have a text content type, just assume this
+                # is a raw post we want to save as a file
+                # and save that as an attachment
+                use_raw_data = True
+            query_params = {} if use_raw_data else request.POST
         type, exc, tb = sys.exc_info()
         traceback_string = "".join(traceback.format_tb(tb))
         record = ExceptionRecord(type=str(type),
@@ -39,6 +45,10 @@ class ExceptionRecord(Document):
                                  url=url,
                                  query_params=query_params)
         record.save()
+        if use_raw_data:
+            record.put_attachment(request.raw_post_data, name="post_data", 
+                                  content_type=request.META["CONTENT_TYPE"],
+                                  content_length=len(request.raw_post_data))
         return record
     
     @classmethod
