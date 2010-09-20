310df09bf8a9961c63f2224a166fde1cf0d6f035
Cory Zue <czue@dimagi.com>
2010-09-20 14:06:57 -0400

    fix django admin media to match our deployments

--- bhoma/settings.py
+++ bhoma/settings.py
@@ -47,7 +47,7 @@ MEDIA_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 't@x8944rd@6!ya-r_+321$*2mlx(mfyohioxeczh5r+^-gc2f2'
