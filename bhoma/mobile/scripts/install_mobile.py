eaa9d668b41b41c5c74a7a7363b4a5cef30c1f13
Drew Roos <droos@dimagi.com>
2010-09-28 19:08:08 -0400

    drew's hacky scripts for building/installing mobile app

---
+++ bhoma/mobile/scripts/install_mobile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import os.path
+
+memcard = '/media/1234-5678'
+builds = '/home/drew/ccbuilds'
+
+if not os.path.exists(memcard):
+  print 'memory card not mounted?'
+  sys.exit()
+
+try:
+  stamp = sys.argv[1]
+except:
+  stamp = sorted(os.listdir(builds))[-1]
+
+builddir = os.path.join(builds, stamp)
+print 'build dir:', builddir
+
+os.popen('rm %s' % os.path.join(memcard, 'CommCare*'))
+os.popen('cp %s %s' % (os.path.join(builddir, '*'), memcard))
+os.popen('umount %s' % memcard)
+
+print 'done'
