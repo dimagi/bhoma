1f4c73d363e1fb4984b4c1dc7b51da6dd61e0548
Cory Zue <czue@dimagi.com>
2010-09-28 14:53:04 -0400

    add initial test for case xml generated by a case that's sent to phone

--- bhoma/apps/case/tests/test_clinic_cases.py
+++ bhoma/apps/case/tests/test_clinic_cases.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from datetime import date, datetime
 from bhoma.apps.patient import export as export
+from django.test.client import Client
 import os
+from django.core.urlresolvers import reverse
+from bhoma.apps.phone.xml import date_to_xml_string
+from bhoma.apps.case.tests.util import check_xml_line_by_line
 
 class ClinicCaseTest(TestCase):
 
@@ -93,3 +97,61 @@ class ClinicCaseTest(TestCase):
         self.assertEqual(date(2010, 10, 4), ccase.due_date)
         self.assertEqual(date(2010, 9, 27), ccase.start_date)
         
+    def testPhoneTest(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "phone_test")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        # forms are loaded one at a time.  If you need to run tests at 
+        # intermediate states, put them in between whatever forms you
+        # want loaded
+
+        updated_patient, form_doc1 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "001_general.xml"))
+        
+        self.assertEqual(1, len(updated_patient.cases))
+        case = updated_patient.cases[0]
+        self.assertFalse(case.closed)
+        self.assertTrue(case.send_to_phone)
+        self.assertEqual("urgent_clinic_followup", case.send_to_phone_reason)
+        ccase = case.commcare_cases[0]
+        self.assertFalse(ccase.closed)
+        self.assertEqual(case.get_id, ccase.external_id)
+        self.assertEqual("missed_appt", ccase.followup_type)
+        self.assertEqual(date(2010, 9, 9), ccase.activation_date)
+        self.assertEqual(date(2010, 9, 16), ccase.due_date)
+        self.assertEqual(date(2010, 9, 9), ccase.start_date)
+        
+        # grab the case xml and check it against what we expect to get back
+        c = Client()
+        response = c.get(reverse("patient_case_xml", args=[updated_patient.get_id]))
+        
+        expected_casexml = \
+"""<case>
+    <case_id>27e45e6086a8418290f82e1494ca54e7</case_id> 
+    <date_modified>%(today)s</date_modified>
+    <create>
+        <case_type_id>bhoma_followup</case_type_id> 
+        <user_id>f4374680-9bea-11df-a4f6-005056c00008</user_id> 
+        <case_name>general|diarrhea</case_name> 
+        <external_id>61a91e70d246486b99abb6ad752e82a5</external_id>
+    </create>
+    <update>
+        <first_name>PHONE</first_name>
+        <last_name>TEST</last_name>
+        <birth_date>1987-03-19</birth_date>
+        <birth_date_est>False</birth_date_est>
+        <age>23</age>
+        <sex>f</sex>
+        <village>GSMLAND</village>
+        <contact>0128674102</contact>
+        <bhoma_case_id>829684277b127eed27e1d6ef08d6c74a</bhoma_case_id>
+        <bhoma_patient_id>829684277b127eed27e1d6ef08d6c74a</bhoma_patient_id>
+        <followup_type>missed_appt</followup_type>
+        <orig_visit_type>general</orig_visit_type>
+        <orig_visit_diagnosis>diarrhea</orig_visit_diagnosis>
+        <orig_visit_date>2010-09-01</orig_visit_date>
+        <activation_date>2010-09-09</activation_date>
+        <due_date>2010-09-16</due_date>
+        <missed_appt_date>2010-09-06</missed_appt_date>
+    </update>
+</case>""" % {"today": date_to_xml_string(date.today())}
+        
+        check_xml_line_by_line(self, expected_casexml, response.content)        
