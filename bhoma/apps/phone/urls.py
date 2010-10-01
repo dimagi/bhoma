ac5cf1c8e6ef770d13d87406ff758bc610dbd1e5
Cory Zue <czue@dimagi.com>
2010-09-30 22:03:39 -0400

    replacing restore url with stub for safety's sake for deployment

--- bhoma/apps/phone/urls.py
+++ bhoma/apps/phone/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',                       
     url(r'^test/$', 'bhoma.apps.phone.views.test'),
-    url(r'^restore/$', 'bhoma.apps.phone.views.restore'),
+    url(r'^restore/$', 'bhoma.apps.phone.views.restore_caseless'),
+    url(r'^restore/real/$', 'bhoma.apps.phone.views.restore'),
     url(r'^post/$', 'bhoma.apps.phone.views.post'),
     url(r'^logs/$', 'bhoma.apps.phone.views.logs'),
     url(r'^case_xml_for_patient/(?P<patient_id>\w+)/$', 'bhoma.apps.phone.views.patient_case_xml',
