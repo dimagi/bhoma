f5c5b2664cc47f17dec3dac9f03a729194500af9
Cory Zue <czue@dimagi.com>
2010-10-06 21:28:08 -0400

    add tests for pregnancy edd conditions

--- bhoma/apps/case/tests/test_pregnancy.py
+++ bhoma/apps/case/tests/test_pregnancy.py
@@ -37,4 +37,48 @@ class PregnancyObjectTest(TestCase):
         self.assertEqual(form_doc1.get_id, preg.anchor_form_id)
         self.assertEqual(2, len(preg.other_form_ids))
         
+    def testHealthyPregnancyLMP(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc1 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "001_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(date(2011,3,8), preg.edd)
+        
+        
     
+    def testHealthyPregnancyEDD(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc2 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "002_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(date(2011,2,10), preg.edd)
+        
+    
+    def testHealthyPregnancyGestationalAge(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc3 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "003_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(date(2011,1,14), preg.edd)
+        
+    def testHealthyPregnancyMisingEDD(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc4 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "004_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(None, preg.edd)
+        
+    def testSickPregnancyGestationalAge(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc5 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "005_sick_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(date(2011,3,21), preg.edd)
+        
+    def testSickPregnancyMissingEDD(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "edd_conditions")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        updated_patient, form_doc6 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "006_sick_pregnancy.xml"))
+        [preg] = updated_patient.pregnancies
+        self.assertEqual(None, preg.edd)
+                
