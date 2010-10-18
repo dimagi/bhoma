db60aebac405b52e370f92460b56d78655774c8d
Cory Zue <czue@dimagi.com>
2010-10-18 11:38:21 -0400

    add commands to fabfile

--- bhoma/fabfile.py
+++ bhoma/fabfile.py
@@ -8,6 +8,31 @@ def test():
 def pack():
     local('tar czf /tmp/bhoma.tgz .', capture=False)
 
+def fetch():
+    """fetch latest code to remote environment """
+    run('git fetch origin master')
+
+def merge():
+    """merge latest code to local environment """
+    run('git merge origin')
+
+
+def pull():
+    """pull latest code to remote environment """
+    run('git pull origin master')
+
+def stop_apache():
+    run("/etc/init.d/apache2 stop")
+
+def start_apache():
+    run("/etc/init.d/apache2 start")
+
+def update():
+    stop_apache()
+    pull()
+    start_apache()
+    
+    
 def prepare_deploy():
     test()
     pack()
@@ -17,3 +42,4 @@ def deploy():
     with cd('/var/src/bhoma/bhoma/'):
         run('tar xzf /tmp/bhoma.tgz')
         
+
