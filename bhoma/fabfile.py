faa43b43ef3bacc9c415ab639384449c1fc548ac
Cory Zue <czue@dimagi.com>
2010-10-18 16:59:25 -0400

    more fab goodies, add update tag method, restore and clean backups on error

--- bhoma/fabfile.py
+++ bhoma/fabfile.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-import tempfile
 from fabric.api import *
 from fabric.contrib.console import confirm
 
 # use this instead of os.path.join since remote OS might differ from local
 PATH_SEP = "/"
+TMP_DIR = "/tmp"
 SOURCE_DIR = "/var/src/bhoma"
-APP_DIR = "%s%sbhoma" % (SOURCE_DIR, PATH_SEP)
+APP_DIR = PATH_SEP.join((SOURCE_DIR, "bhoma"))
 BACKUP_DIR = "/var/src/backups"
 
 def central():
@@ -42,6 +42,14 @@ def fetch():
     """fetch latest code to remote environment """
     sudo('git fetch %(repo)s master' % {"repo": env.repo_name} )
 
+def fetch_tags():
+    """fetch latest tags to remote environment """
+    sudo('git fetch --tags %(repo)s master' % {"repo": env.repo_name} )
+
+def checkout_tag(tagname):
+    """Checks out a tag"""
+    sudo('git checkout %(tagname)s' % {"tagname": tagname } )
+
 def merge():
     """merge latest code to local environment """
     sudo('git merge %(repo)s' % {"repo": env.repo_name} )
@@ -76,10 +84,10 @@ def remove_directory(target):
     sudo("rm -R %(target)s" % {"target": target})
 
 def timestamp_string():
-    return datetime.now().strftime("%Y-%m-%d-%H:%m:%S:%f")
+    return datetime.now().strftime("%Y-%m-%d-%H.%m.%S.%f")
 
 def restore_directory(src, target):
-    tmpdir = os.path.join(tempfile.gettempdir(), timestamp_string())
+    tmpdir = PATH_SEP.join((TMP_DIR, timestamp_string()))
     move_directory(target, tmpdir)
     backup_directory(src, target)
     remove_directory(src)
@@ -88,10 +96,10 @@ def noop():
     require('root', provided_by=('central', 'dimagi', 'clinic'))
     run("echo 'hello world'")
     
-def update():
+def update_latest():
     require('root', provided_by=('central', 'dimagi', 'clinic'))
     require('repo_name', provided_by=('central', 'dimagi', 'clinic'))
-    backup_dir = "%s%s%s" % (BACKUP_DIR, PATH_SEP, timestamp_string())
+    backup_dir = PATH_SEP.join((BACKUP_DIR, timestamp_string()))
     backup_directory(SOURCE_DIR, backup_dir)
     with cd(APP_DIR):
         stop_apache()
@@ -100,6 +108,29 @@ def update():
         syncdb()
         start_formplayer()
         start_apache()
+    
+def update_tag(tagname):
+    require('root', provided_by=('central', 'dimagi', 'clinic'))
+    require('repo_name', provided_by=('central', 'dimagi', 'clinic'))
+    backup_dir = "%s%s%s" % (BACKUP_DIR, PATH_SEP, timestamp_string())
+    backup_directory(SOURCE_DIR, backup_dir)
+    with cd(APP_DIR):
+        stop_apache()
+        stop_formplayer()
+        try:
+            fetch()
+            fetch_tags()
+            checkout_tag(tagname)
+            syncdb()
+            remove_directory(backup_dir)
+        except SystemExit:
+            print "exception"
+            with cd(TMP_DIR):
+                restore_directory(backup_dir, SOURCE_DIR)
+        finally:
+            print "finally"
+            start_formplayer()
+            start_apache()
     
 def prepare_deploy():
     test()
