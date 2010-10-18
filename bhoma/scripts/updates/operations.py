cf04b2e4e63868848a8684b310a92b23f000d1f6
Cory Zue <czue@dimagi.com>
2010-10-18 10:36:48 -0400

    fix move command

--- bhoma/scripts/updates/operations.py
+++ bhoma/scripts/updates/operations.py
@@ -20,13 +20,20 @@ def verify_status(status, msg, callback_fail=sys.exit, callback_success=noop):
 def simulate_failure():
     return 1
 
-def git_fetch():
+def _push_pop_dir(directory, command):
+    return "cd %(dir)s && %(cmd)s && cd -" % {"dir": directory, "cmd" : command}
+
+def git_fetch(directory=None):
     GIT_FETCH_COMMAND = "git fetch origin master"
+    if directory:
+        GIT_FETCH_COMMAND = _push_pop_dir(directory, GIT_FETCH_COMMAND)
     proc = Popen(GIT_FETCH_COMMAND, stderr=PIPE, stdout=PIPE, shell=True)
     return proc.wait()
 
-def git_merge():
+def git_merge(directory=None):
     GIT_MERGE_COMMAND = "git merge origin"
+    if directory:
+        GIT_MERGE_COMMAND = _push_pop_dir(directory, GIT_MERGE_COMMAND)
     proc = Popen(GIT_MERGE_COMMAND, stderr=PIPE, stdout=PIPE, shell=True)
     return proc.wait()
 
@@ -37,7 +44,7 @@ def backup_directory(src, target):
     return proc.wait()
 
 def move_directory(src, target):
-    MV_COMMAND = "mv -Rp %(src)s %(target)s"
+    MV_COMMAND = "mv %(src)s %(target)s"
     print "moving %(src)s to %(target)s" % {"src": src, "target": target}
     proc = Popen(MV_COMMAND % {"src": src, "target": target}, stderr=PIPE, stdout=PIPE, shell=True)
     return proc.wait()
