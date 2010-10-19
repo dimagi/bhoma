a260c0da69c811f277d5e2015dc3191443ff93f4
Drew Roos <droos@dimagi.com>
2010-10-19 17:20:35 -0400

    update build scripts

--- bhoma/mobile/scripts/bhomahash.py
+++ bhoma/mobile/scripts/bhomahash.py
@@ -4,10 +4,14 @@ import os
 import os.path
 import sys
 
-gitdir = os.path.join(sys.argv[1], '.git')
+def gethash(bhoma_root):
+  gitdir = os.path.join(bhoma_root, '.git')
 
-try:
-  lns = os.popen('git --git-dir %s show .' % gitdir).readlines()
-  print lns[0].split()[1][:12]
-except:
-  print 'error'
+  try:
+    lns = os.popen('git --git-dir %s show .' % gitdir).readlines()
+    return lns[0].split()[1][:12]
+  except:
+    return 'error'
+
+if __name__ == "__main__":
+  print gethash(sys.argv[1])
