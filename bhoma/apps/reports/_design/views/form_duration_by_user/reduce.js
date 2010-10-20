c5a1c047e823ff7b0eb7599e57372e548d88d778
Cory Zue <czue@dimagi.com>
2010-10-20 14:56:13 -0400

    use builtin _stats reduce function

--- bhoma/apps/reports/_design/views/form_duration_by_user/reduce.js
+++ bhoma/apps/reports/_design/views/form_duration_by_user/reduce.js
@@ -1,14 +1,1 @@
-function (keys, values, rereduce) {
-    if (!rereduce) {
-        return {"sum" : sum(values), "count" : values.length};
-    }
-    else {
-		count = 0;
-		sum = 0;
-		for (i in values) {
-		  count += values[i]['count'];
-		  sum += values[i]['sum'];
-		}
-		return {"sum" : sum, "count" : count};
-    }
-}
+_stats
