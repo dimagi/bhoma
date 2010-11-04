1d077f4351b433fd7247a469eac94454a3e09bdb
Cory Zue <czue@dimagi.com>
2010-11-04 16:16:53 -0400

    add lucene search option for couchlogs.

--- bhoma/apps/couchlog/views.py
+++ bhoma/apps/couchlog/views.py
@@ -13,7 +13,8 @@ from django.template.loader import render_to_string
 from bhoma.apps.djangocouch.utils import futon_url
 from django.utils.text import truncate_words
 from bhoma.apps.locations.models import Location
-from bhoma.utils.couch.pagination import CouchPaginator
+from bhoma.utils.couch.pagination import CouchPaginator, LucenePaginator
+from couchdbkit.client import View
 
 def dashboard(request):
     """
@@ -27,8 +28,16 @@ def dashboard(request):
         if op == "bulk_archive":
             query = request.POST.get("query", "")
             if query:
-                # TODO: bulk archive
-                records = ExceptionRecord.view("couchlog/inbox_by_msg", reduce=False, key=query).all()
+                def get_matching_records(query):
+                    if settings.LUCENE_ENABLED:
+                        query = "%s AND NOT archived" % query
+                        limit = get_db().search("couchlog/search", q=query, limit=1).total_rows
+                        matches = get_db().search("couchlog/search", q=query, limit=limit, include_docs=True)
+                        return [ExceptionRecord.wrap(res["doc"]) for res in matches]
+                        
+                    else:
+                        return ExceptionRecord.view("couchlog/inbox_by_msg", reduce=False, key=query).all() 
+                records = get_matching_records(query)
                 for record in records:
                     record.archived = True
                 ExceptionRecord.bulk_save(records)    
@@ -50,6 +59,39 @@ def single(request, log_id):
                               {"log": log},
                               context_instance=RequestContext(request))
 
+
+def _record_to_json(error):
+    """Shared by the wrappers"""
+    def truncate(message, length=100, append="..."):
+        if length < len(append):
+            raise Exception("Can't truncate to less than %s characters!" % len(append))
+        return "%s%s" % (message[:length], append) if len(message) > length else message
+     
+    return [error.get_id,
+            error.archived, 
+            getattr(error, "clinic_id", "UNKNOWN"), 
+            error.date.strftime('%Y-%m-%d %H:%M:%S') if error.date else "", 
+            error.type, 
+            truncate(error.message), 
+            error.url,
+            "archive",
+            "email"]
+    
+
+def lucene_search(request, search_key, show_all):
+    
+    def wrapper(row):
+        id = row["id"]
+        doc = ExceptionRecord.get(id)
+        return _record_to_json(doc)
+    
+    if not show_all:
+        search_key = "%s AND NOT archived" % search_key
+    total_records = get_db().view("couchlog/count").one()["value"]
+    paginator = LucenePaginator("couchlog/search", wrapper)
+    return paginator.get_ajax_response(request, search_key, extras={"iTotalRecords": total_records})
+                                    
+    
 def paging(request):
     
     # what to show
@@ -58,6 +100,8 @@ def paging(request):
     search_key = query.get("sSearch", "")
     show_all = query.get("show", "inbox") == "all"
     if search_key:
+        if settings.LUCENE_ENABLED:
+            return lucene_search(request, search_key, show_all)
         view_name = "couchlog/all_by_msg" if show_all else "couchlog/inbox_by_msg"
         search = True
     else:
@@ -69,22 +113,8 @@ def paging(request):
         Given a row of the view, get out an exception record
         """
         error = ExceptionRecord.wrap(row["value"])
+        return _record_to_json(error)
         
-        def truncate(message, length=100, append="..."):
-            if length < len(append):
-                raise Exception("Can't truncate to less than %s characters!" % len(append))
-            return "%s%s" % (message[:length], append) if len(message) > length else message
-         
-        return [error.get_id,
-                error.archived, 
-                getattr(error, "clinic_id", "UNKNOWN"), 
-                error.date.strftime('%Y-%m-%d %H:%M:%S') if error.date else "", 
-                error.type, 
-                truncate(error.message), 
-                error.url,
-                "archive",
-                "email"]
-    
     paginator = CouchPaginator(view_name, wrapper_func, search=search)
     
     # get our previous start/end keys if necessary
