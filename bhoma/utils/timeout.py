39752055da4e5d7d978daadaf1e8fb2839ce2e0d
Drew Roos <droos@dimagi.com>
2010-11-12 15:33:03 -0500

    make restore request time out fast

---
+++ bhoma/utils/timeout.py
@@ -0,0 +1,33 @@
+import threading
+
+class TimeoutException(Exception):
+    pass
+
+class TimeoutTask(threading.Thread):
+    def __init__(self, func, args, kwargs, cachefunc=None):
+        threading.Thread.__init__(self)
+        self.eval = lambda: func(*args, **kwargs)
+        self.cachefunc = cachefunc
+        self.expired = False
+        self.lock = threading.Lock()
+
+    def run(self):
+        self.result = self.eval()
+        with self.lock:
+            if self.expired and self.cachefunc:
+                self.cachefunc(self.result)
+
+    def execute(self, timeout=None):
+        self.start()
+        self.join(timeout)
+        with self.lock:
+            if self.isAlive():
+                self.expired = True
+                raise TimeoutException()
+            else:
+                return self.result
+
+def timeout(n):
+    def timeout_n(f):
+        return lambda *args, **kwargs: TimeoutTask(f, args, kwargs).execute(n)
+    return timeout_n
