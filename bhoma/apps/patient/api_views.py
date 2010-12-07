a229340b802a6604b026e76648e364b695189a23
Drew Roos <droos@dimagi.com>
2010-12-07 18:23:07 -0500

    restrict access to patient-identifiable data; bump version #

--- bhoma/apps/patient/api_views.py
+++ bhoma/apps/patient/api_views.py
@@ -8,7 +8,7 @@ from bhoma.utils.couch.pagination import CouchPaginator
 from bhoma.const import VIEW_ALL_PATIENTS, VIEW_PATIENT_SEARCH,\
     VIEW_PATIENT_BY_LAST_NAME, VIEW_PATIENT_BY_BHOMA_ID
 
-
+@restricted_patient_data
 def lookup_by_id(request):
     """
     Get a patient by ID, returning the json representation of the patient
@@ -18,6 +18,7 @@ def lookup_by_id(request):
     json_pats = [pat.to_json() for pat in patients]
     return HttpResponse(json.dumps(json_pats), mimetype='text/json')
 
+@restricted_patient_data
 def fuzzy_match(request):
     # TODO this currently always returns nothing
     fname = request.POST.get('fname')
@@ -28,6 +29,7 @@ def fuzzy_match(request):
     return HttpResponse(json.dumps(None), mimetype='text/plain')
 
 
+@restricted_patient_data
 def paging(request):
     """
     Paging view, used by datatables url
@@ -52,6 +54,7 @@ def paging(request):
                                search=True, search_preprocessor=id_formatter)
     return paginator.get_ajax_response(request)
 
+@restricted_patient_data
 def paging_identified(request):
     """
     Paging view, used by datatables url
