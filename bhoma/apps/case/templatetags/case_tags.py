6befcde0141cb5c409648a175ee4d24bf769e757
Cory Zue <czue@dimagi.com>
2010-12-22 14:15:10 -0500

    small tweaks to single patient layout

--- bhoma/apps/case/templatetags/case_tags.py
+++ bhoma/apps/case/templatetags/case_tags.py
@@ -7,6 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def format_case(case):
-    template = '<span class="%(status)s">%(status)s: %(modifier)s</span>'
+    template = '<span class="%(status)s">%(type)s / %(modifier)s</span>'
     status = "closed" if case.closed else "open"
-    return template % {"status": status, "modifier": case.status_display()}
+    return template % {"status": status, "modifier": case.status_display(), 
+                       "type": case.type}
