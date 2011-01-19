e741b180953f6b6f0592a72f695e0bb5e01c6ab6
Cory Zue <czue@dimagi.com>
2011-01-19 14:39:09 -0500

    add noinput flag to deleting a database

--- bhoma/apps/djangocouch/management/commands/delete_db.py
+++ bhoma/apps/djangocouch/management/commands/delete_db.py
@@ -9,6 +9,7 @@ from bhoma.utils.parsing import string_to_boolean
 from bhoma.apps.patient.management.commands.shared import are_you_sure
 import sys
 from couchdbkit.resource import ResourceNotFound
+from optparse import make_option
 
 WARNING = \
 """About to delete database %s.  This operation is PERMANENT and IRREVERSIBLE.
@@ -18,12 +19,16 @@ class Command(LabelCommand):
     help = "Deletes a couch db."
     args = "db"
     label = ""
-     
+    option_list = LabelCommand.option_list + \
+        (make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind. '    \
+                 'Databases will be deleted without warning.'),)
+
     def handle(self, *args, **options):
         if len(args) != 1:
             raise CommandError('Usage: manage.py delete_db <database>')
         dbname = args[0]
-        should_proceed = are_you_sure(WARNING % dbname)
+        should_proceed = are_you_sure(WARNING % dbname) if options["interactive"] else True
         if not should_proceed:
             print "Ok, aborting"
             sys.exit()
