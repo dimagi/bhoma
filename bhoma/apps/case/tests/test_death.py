d04ac1fe7364562b1d0c4a9d77bdca84954bdaab
Cory Zue <czue@dimagi.com>
2011-01-20 17:37:57 -0500

    move death outcome test to the test_death class

--- bhoma/apps/case/tests/test_death.py
+++ bhoma/apps/case/tests/test_death.py
@@ -9,6 +9,7 @@ class DeathTest(TestCase):
         folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "death_after")
         patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
         
+        self.assertFalse(patient.is_deceased)
         # enter a form with outcome death
         updated_patient, form_doc1 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "001_general.xml"))
         # enter a form with a different outcome that should create a case
@@ -20,3 +21,17 @@ class DeathTest(TestCase):
             self.assertTrue(case.closed)
             self.assertEqual(Outcome.PATIENT_DIED, case.outcome)
             
+    def testDeath(self):
+        folder_name = os.path.join(os.path.dirname(__file__), "testpatients", "death_test")
+        patient = export.import_patient_json_file(os.path.join(folder_name, "patient.json"))
+        self.assertFalse(patient.is_deceased)
+        updated_patient, form_doc1 = export.add_form_file_to_patient(patient.get_id, os.path.join(folder_name, "001_general.xml"))
+        self.assertTrue(updated_patient.is_deceased)
+        [case] = updated_patient.cases
+        self.assertTrue(case.closed)
+        self.assertTrue(updated_patient.is_deceased)
+        self.assertEqual("cardiac_failure", case.type)
+        self.assertEqual(Outcome.PATIENT_DIED, case.outcome)
+        self.assertEqual(0, len(case.commcare_cases))
+    
+    
