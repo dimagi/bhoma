61f4c6b015803bc3db96738d6074098c64b8370f
Cory Zue <czue@dimagi.com>
2011-01-21 18:21:34 -0500

    make sure to sync migration design doc in source db before migrating to rev2

--- bhoma/apps/migration/management/commands/migrate_rev2_data.py
+++ bhoma/apps/migration/management/commands/migrate_rev2_data.py
@@ -1,10 +1,11 @@
 from django.core.management.base import LabelCommand, CommandError
 from bhoma.utils.couch.database import get_db
-from bhoma.apps.patient.management.commands.shared import are_you_sure
-import sys
 from couchdbkit.resource import ResourceNotFound
 from couchdbkit.client import Database
 from bhoma.utils.couch.sync import replicate
+import os
+from django.conf import settings
+from couchdbkit.loaders import FileSystemDocLoader
 
 class Command(LabelCommand):
     help = "Migrates some data to a new rev2 database."
@@ -16,6 +17,14 @@ class Command(LabelCommand):
             raise CommandError('Usage: manage.py migrate_rev2_data <old_db>')
         db = get_db()
         source_db = args[0]
+        # the source db needs to have the migration views built so just force-sync the 
+        # design doc
+        print "prepping source database %s" % source_db
+        source = db.server.get_or_create_db(source_db)
+        path_to_migration = os.path.join(settings.BHOMA_ROOT_DIR, "apps", "migration")
+        loader = FileSystemDocLoader(path_to_migration, "_design", design_name="migration")
+        loader.sync(source)
+        
         print "migrating rev1 data to from %s to %s" % (source_db, db.dbname)
         replicate(db.server, source_db, db.dbname, filter="migration/migrates_rev_2")
         print "migration complete"
