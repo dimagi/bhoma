e0dbff7a5304fd5237c4ab965107cdfaa002b888
Cory Zue <czue@dimagi.com>
2011-02-09 16:02:28 -0500

    deglobalize javascript variables

--- bhoma/apps/webapp/static/javascripts/login.js
+++ bhoma/apps/webapp/static/javascripts/login.js
@@ -3,13 +3,13 @@ function wfLogin () {
   var flow = function (data) {
     var q_username = qUsernameList();
     yield q_username;
-    username = q_username.value;
+    var username = q_username.value;
     
     //enter password
     var auth_validation = function(password) {
-        auth_fail_text = "Sorry, that is not the right password for " + username + ". Please try again.";
-        error_text = "Sorry, something went wrong. If this keeps happening please contact CIDRZ.  Your message is: ";        
-        auth_res = jQuery.ajax({url: '/api/auth/', 
+        var auth_fail_text = "Sorry, that is not the right password for " + username + ". Please try again.";
+        var error_text = "Sorry, something went wrong. If this keeps happening please contact CIDRZ.  Your message is: ";        
+        var auth_res = jQuery.ajax({url: '/api/auth/', 
                                 type: 'POST', 
                                 data: {'username': username, 'password': password}, 
                                 async: false,
