5110a33fec2d5472e45476d8a35046ab0c8a689a
Cory Zue <czue@dimagi.com>
2011-02-27 18:59:16 -0500

    usr couchlog, add staticfiles workaround

--- bhoma/settings.py
+++ bhoma/settings.py
@@ -67,6 +67,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
     "bhoma.context_processors.webapp",
+    "couchlog.context_processors.static_workaround",
 )
 
 MIDDLEWARE_CLASSES = (
