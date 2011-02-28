41cc33556e3e599ae17b9367d906a0b33c7a8bb9
Cory Zue <czue@dimagi.com>
2011-02-28 16:56:33 -0500

    fix tests

--- bhoma/apps/bhomalog/tests.py
+++ bhoma/apps/bhomalog/tests.py
@@ -7,16 +7,16 @@ import logging
 class BhomaLogTestCase(TestCase):
     
     def setUp(self):
-        for item in ExceptionRecord.view("couchlog/all_by_date").all():
+        for item in ExceptionRecord.view("couchlog/all_by_date", include_docs=True).all():
             item.delete()
     
     def testSettingsInfo(self):
-        self.assertEqual(0, len(ExceptionRecord.view("couchlog/all_by_date").all()))
+        self.assertEqual(0, len(ExceptionRecord.view("couchlog/all_by_date", include_docs=True).all()))
         try:
             raise Exception("Fail!")
         except Exception, e:
             logging.exception(e)
-        log = ExceptionRecord.view("couchlog/all_by_date").one()
+        log = ExceptionRecord.view("couchlog/all_by_date", include_docs=True).one()
         self.assertEqual(settings.APP_VERSION, log.app_version)
         self.assertEqual(settings.BHOMA_COMMIT_ID, log.commit_id)
         
