bcde4c5fe108662cd759dee6b79e039eea8dc198
Cory Zue <czue@dimagi.com>
2011-03-23 14:25:15 -0400

    convert the phone views to always use iso date emits

--- bhoma/apps/phone/_design/views/cases_sent_to_chws/map.js
+++ bhoma/apps/phone/_design/views/cases_sent_to_chws/map.js
@@ -1,7 +1,8 @@
-function(doc) { 
+function(doc) {
+    // !code util/dates.js
     if (doc.doc_type == "SyncLog") {
         for (i in doc.cases) {
-            emit([doc.chw_id, doc.cases[i]], new Date(doc.date));
+            emit([doc.chw_id, doc.cases[i]], iso_date_string(new Date(doc.date)));
         }
     }
 }
--- bhoma/apps/phone/_design/views/sync_logs_by_chw/map.js
+++ bhoma/apps/phone/_design/views/sync_logs_by_chw/map.js
@@ -1,5 +1,7 @@
 function(doc) { 
     if (doc.doc_type == "SyncLog") {
+        // note that we don't have to convert this to iso because
+        // it's already in JSON format
         emit([doc.chw_id, doc.date], 1);
     }
 }
