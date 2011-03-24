9a487e7856fddbb8494d431396fe1e57ddb59975
Cory Zue <czue@dimagi.com>
2011-03-24 12:37:08 -0400

    only emit cases that have dates in the cases_sent_by_date view.

--- bhoma/apps/phone/_design/views/cases_sent_to_chws/map.js
+++ bhoma/apps/phone/_design/views/cases_sent_to_chws/map.js
@@ -1,8 +1,9 @@
 function(doc) {
-    // !code util/dates.js
     if (doc.doc_type == "SyncLog") {
         for (i in doc.cases) {
-            emit([doc.chw_id, doc.cases[i]], doc.date);
+            if (doc.date) {
+                emit([doc.chw_id, doc.cases[i]], doc.date);
+            }
         }
     }
 }
