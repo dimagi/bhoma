dd51e18cca63db14f59a4e7a09a95bb547e60aec
Cory Zue <czue@dimagi.com>
2011-03-24 12:38:49 -0400

    fix iso date parse in a reduce function that i missed

--- bhoma/apps/phone/_design/views/cases_sent_to_chws/reduce.js
+++ bhoma/apps/phone/_design/views/cases_sent_to_chws/reduce.js
@@ -1,10 +1,12 @@
 function(keys, values, rereduce) {
+  // !code util/dates.js
+    
   // according to https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date
   // and http://stackoverflow.com/questions/3009810/php-mysql-javascript-mindate-and-maxdate
   // this is the biggest date in javascript.  we probably could get away with 9999
   var cur_min = new Date( 100000000*86400000);  
   for (i in values) {
-     dt = new Date(values[i]);
+     dt = parse_iso_date(values[i]);
      if (dt < cur_min) {
         cur_min = dt;
      }
