4d7e8badd10943fd816300563911bf0719c4865a
Cory Zue <czue@dimagi.com>
2011-04-11 06:00:20 -0700

    update string_to_datetime test

--- bhoma/apps/patient/tests/test_utils.py
+++ bhoma/apps/patient/tests/test_utils.py
@@ -5,4 +5,4 @@ from dimagi.utils.parsing import string_to_datetime
 class UtilsTestCase(TestCase):
     
     def testDateParsing(self):
-        self.assertEqual(datetime(2010,01,15), string_to_datetime("2010-01-15"))
+        self.assertEqual(datetime(1965,7,20), string_to_datetime("1965-07-20"))
