83c2b5cff9c97342596d8ddb7ff60a1adcb4070f
Cory Zue <czue@dimagi.com>
2011-04-11 06:01:26 -0700

    support urls with case ids with hyphens

--- bhoma/apps/case/urls.py
+++ bhoma/apps/case/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^duplicates/$', 'bhoma.apps.case.views.duplicates', name='duplicate_cases'),
-    url(r'^duplicates/(?P<case_id>\w+)/$', 'bhoma.apps.case.views.duplicate_details', 
+    url(r'^duplicates/(?P<case_id>[\w-]+)/$', 'bhoma.apps.case.views.duplicate_details', 
         name='duplicate_case_details'),
 )
