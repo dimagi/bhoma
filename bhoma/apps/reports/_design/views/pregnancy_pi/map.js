5aa1a5f423cf096ce88c7a4d107b93a610065ab0
Cory Zue <czue@dimagi.com>
2011-06-02 18:27:50 -0400

    clean up pregnancy pi report

--- bhoma/apps/reports/_design/views/pregnancy_pi/map.js
+++ bhoma/apps/reports/_design/views/pregnancy_pi/map.js
@@ -5,10 +5,11 @@ function(doc) {
 	// !code util/dates.js
     // !code util/reports.js
     // !code util/xforms.js
-	HEALTHY_NAMESPACE = "http://cidrz.org/bhoma/pregnancy";
-    SICK_NAMESPACE = "http://cidrz.org/bhoma/sick_pregnancy";
-    DELIVERY_NAMESPACE = "http://cidrz.org/bhoma/delivery";
-    enc_date = get_encounter_date(doc);
+	
+	var HEALTHY_NAMESPACE = "http://cidrz.org/bhoma/pregnancy";
+    var SICK_NAMESPACE = "http://cidrz.org/bhoma/sick_pregnancy";
+    var DELIVERY_NAMESPACE = "http://cidrz.org/bhoma/delivery";
+    var enc_date = get_encounter_date(doc);
     
     function _emit(name, num, denom) {
         emit([enc_date.getFullYear(), enc_date.getMonth(), doc.meta.clinic_id, name], [num, denom]);    
@@ -22,7 +23,6 @@ function(doc) {
             return;
         }
     
-        report_values = [];
         /* this field keeps track of total forms */
         _emit("total",1,1);
         
@@ -33,7 +33,7 @@ function(doc) {
         #Proportion of routine visits with Blood Pressure and Urinalysis Protein results
         */
         
-        vitals_recorded_num = Boolean(doc.blood_pressure && (doc.urinalysis_protein == "p" || doc.urinalysis_protein == "n")) ? 1 : 0;
+        var vitals_recorded_num = Boolean(doc.blood_pressure && (doc.urinalysis_protein == "p" || doc.urinalysis_protein == "n")) ? 1 : 0;
         _emit("preeclamp", vitals_recorded_num, 1);
         
         /*
@@ -41,30 +41,28 @@ function(doc) {
         #3. Clinical Exam: record Fundal Height, Presentation, and Fetal Heart Rate
         */
         
-        gest_age = doc.gestational_age;
+        var gest_age = doc.gestational_age;
+        var clinic_exam_num = 0;
+        var clinic_exam_denom = 0;
         if (parseInt(gest_age) > 28) {
         	clinic_exam_num = Boolean(doc.fundal_height && doc.fetal_heart_rate && doc.presentation != "blank") ? 1 : 0;
         	clinic_exam_denom = 1;
         } else if (parseInt(gest_age) > 16) {
         	clinic_exam_num = Boolean(doc.fundal_height && doc.fetal_heart_rate) ? 1 : 0;
         	clinic_exam_denom = 1;
-        } else {
-        	clinic_exam_num = 0;
-        	clinic_exam_denom = 0;
-    	}
+        } 
         _emit("clin_exam", clinic_exam_num, clinic_exam_denom);
         
         /*
 	    #----------------------------------
 	    #4. HIV Testing: Proportion of all pregnant women seen with HIV test done on 1st visit
 		*/
+		var hiv_test_num = 0;
+        var hiv_test_denom = 0;
 		if (doc.visit_number == 1) {
 			hiv_test_denom = 1;
 			hiv_test_num = (doc.hiv_result == "r" || doc.hiv_result == "nr") ? 1 : 0;
-		} else {
-        	hiv_test_num = 0;
-        	hiv_test_denom = 0;
-    	}
+		} 
 		_emit("hiv_test", hiv_test_num, hiv_test_denom);
     } 
     else if (xform_matches(doc, SICK_NAMESPACE)) {
@@ -81,14 +79,13 @@ function(doc) {
         /* this field keeps track of total forms */
         _emit("total",1,1);
         
-        drugs = doc.drugs["prescribed"]["med"];
+        var drugs = doc.drugs.prescribed.med;
+        var drug_stock_denom = 0;
+        var drug_stock_num = 0;
 		if (drugs) {
 	       drug_stock_denom = 1;
 	       drug_stock_num = check_drug_stock(drugs);
-	    } else {
-	       drug_stock_denom = 0;
-	       drug_stock_num = 0;
-	    }
+	    } 
         
         _emit("drugs_stocked", drug_stock_num, drug_stock_denom);
 
@@ -108,18 +105,17 @@ function(doc) {
         # for hiv-positive women not already on Haart, antiretroviral given
         */
         
-        drugs_prescribed = doc.drugs_prescribed;
-        if (doc.history != null && doc.history["hiv_result"] == "r" && doc.history["on_haart"] != "y") {
+        var drugs_prescribed = doc.drugs_prescribed;
+        var maternal_hiv_num = 0;
+        var maternal_hiv_denom = 0;
+        if (doc.history != null && doc.history.hiv_result == "r" && doc.history.on_haart != "y") {
         	if(drugs_prescribed) {
         		maternal_hiv_num = check_drug_type(drugs_prescribed,"antiretroviral");
     		} else {
     			maternal_hiv_num = 0;
     		}
         	maternal_hiv_denom = 1;
-        } else {
-        	maternal_hiv_num = 0;
-        	maternal_hiv_denom = 0;
-        }
+        } 
         _emit("maternal_hiv", maternal_hiv_num, maternal_hiv_denom);
 
     	/*
@@ -128,13 +124,12 @@ function(doc) {
         # infant ARV NVP ticked if mother is Reactive
         */
         
-        if (doc.history["hiv_result"] == "r" && doc.continue_form == "y") {
-        	infant_hiv_num = exists(doc.newborn_eval["infant_arvs"],"nvp") ? 1 : 0;
+        var infant_hiv_num = 0;
+        var infant_hiv_denom = 0;
+        if (doc.history.hiv_result == "r" && doc.continue_form == "y") {
+        	infant_hiv_num = exists(doc.newborn_eval.infant_arvs,"nvp") ? 1 : 0;
         	infant_hiv_denom = 1;
-        } else {
-        	infant_hiv_num = 0;
-        	infant_hiv_denom = 0;
-        }
+        } 
         _emit("infant_nvp", infant_hiv_num, infant_hiv_denom);
         
         /*
@@ -142,49 +137,50 @@ function(doc) {
         #14. Correct management of intrapartum complications
         */
         // TODO: make this function smarter
-        complications = doc.complications;
+        var complications = doc.complications;
         if (complications == null) {
             // no complications, no data
             _emit("del_mgmt", 0, 0);
         } else {
 	        var severe_delivery_symptom = function(doc) {
-		       return (exists(complications["prolonged_labour"], "sev_fhr") || 
-		               exists(complications["hypertension"],"sev_sbp") ||
-		               exists(complications["hypertension"],"sev_dbp") ||
-					   exists(complications["hypertension"],"sev_urine") ||
-		               exists(complications["hypertension"],"sev_hgb") ||
-		               exists(complications["hypertension"], "sev_seizure") ||
-		               exists(complications["hypertension"], "sev_altered") ||
-		               exists(complications["hypertension"], "sev_headache") ||
-		               exists(complications["hypertension"], "sev_visual_changes") ||
-		               exists(complications["hypertension"], "sev_abd_pain") ||
-		               exists(complications["fever"], "sev_sbp") ||
-		               exists(complications["fever"], "sev_resp_rate") ||
-		               exists(complications["fever"], "sev_mat_hr") ||
-		               exists(complications["fever"], "sev_altered") ||
-		               exists(complications["vag_bleed"], "sev_sbp") ||
-		               exists(complications["vag_bleed"], "sev_resp_rate") ||
-		               exists(complications["vag_bleed"], "sev_mat_hr") ||
-		               exists(complications["vag_bleed"], "sev_hgb") ||
-		               exists(complications["vag_bleed"], "sev_altered") ||
-		               exists(complications["mem_rupture"], "sev_sbp") ||
-		               exists(complications["mem_rupture"], "sev_resp_rate") ||
-		               exists(complications["mem_rupture"], "sev_mat_hr") ||
-		               exists(complications["mem_rupture"], "sev_temp") ||
-		               exists(complications["mem_rupture"], "sev_34_weeks") ||
-		               exists(complications["mem_rupture"], "sev_28_weeks") ||
-		               exists(complications["mem_rupture"], "sev_abd_pain") ||
-		               exists(complications["mem_rupture"], "sev_foul_discharge"));
+		       return (exists(doc.complications.prolonged_labour, "sev_fhr") || 
+		               exists(doc.complications.hypertension,"sev_sbp") ||
+		               exists(doc.complications.hypertension,"sev_dbp") ||
+					   exists(doc.complications.hypertension,"sev_urine") ||
+		               exists(doc.complications.hypertension,"sev_hgb") ||
+		               exists(doc.complications.hypertension, "sev_seizure") ||
+		               exists(doc.complications.hypertension, "sev_altered") ||
+		               exists(doc.complications.hypertension, "sev_headache") ||
+		               exists(doc.complications.hypertension, "sev_visual_changes") ||
+		               exists(doc.complications.hypertension, "sev_abd_pain") ||
+		               exists(doc.complications.fever, "sev_sbp") ||
+		               exists(doc.complications.fever, "sev_resp_rate") ||
+		               exists(doc.complications.fever, "sev_mat_hr") ||
+		               exists(doc.complications.fever, "sev_altered") ||
+		               exists(doc.complications.vag_bleed, "sev_sbp") ||
+		               exists(doc.complications.vag_bleed, "sev_resp_rate") ||
+		               exists(doc.complications.vag_bleed, "sev_mat_hr") ||
+		               exists(doc.complications.vag_bleed, "sev_hgb") ||
+		               exists(doc.complications.vag_bleed, "sev_altered") ||
+		               exists(doc.complications.mem_rupture, "sev_sbp") ||
+		               exists(doc.complications.mem_rupture, "sev_resp_rate") ||
+		               exists(doc.complications.mem_rupture, "sev_mat_hr") ||
+		               exists(doc.complications.mem_rupture, "sev_temp") ||
+		               exists(doc.complications.mem_rupture, "sev_34_weeks") ||
+		               exists(doc.complications.mem_rupture, "sev_28_weeks") ||
+		               exists(doc.complications.mem_rupture, "sev_abd_pain") ||
+		               exists(doc.complications.mem_rupture, "sev_foul_discharge"));
 		    }
 	       
-	        comp_deliv_denom = 0;
-	        mgmt_good_so_far = 1;
+	        var comp_deliv_denom = 0;
+	        var mgmt_good_so_far = 1;
+	        var comp_deliv_num = 0;
 	        if (mgmt_good_so_far == 1 && comp_deliv_denom == 0 && (exists(doc.secondary_diagnosis, "uterine_infection") || doc.diagnosis == "uterine_infection") && drugs_prescribed) {
 	        	comp_deliv_denom = 1;
 	        	comp_deliv_num = check_drug_type(drugs_prescribed,"antibiotic") ? 1 : 0;
 	        	mgmt_good_so_far = comp_deliv_num;
 	        }
-	        if (mgmt_good_so_far == 1 && complications["vag_bleed"] && !exists(complications["vag_bleed"],"blank")) {
+	        if (mgmt_good_so_far == 1 && complications.vag_bleed && !exists(complications.vag_bleed,"blank")) {
 	        	comp_deliv_denom = 1;
 	        	if (drugs_prescribed) {
 	        		comp_deliv_num = (exists(doc.other_treatment,"oxygen") && (exists(doc.other_treatment,"fluids") || check_drug_name(drugs_prescribed,"sodium_chloride") || check_drug_name(drugs_prescribed,"ringers_lactate"))) ? 1 : 0;
@@ -193,7 +189,7 @@ function(doc) {
 	        	}
 	        	mgmt_good_so_far = comp_deliv_num;
 	        }
-	        if (mgmt_good_so_far == 1 && doc.phys_exam["fetal_heart_rate"] <= 110 && drugs_prescribed) {
+	        if (mgmt_good_so_far == 1 && doc.phys_exam.fetal_heart_rate <= 110 && drugs_prescribed) {
 	        	comp_deliv_denom = 1;
 	        	if (drugs_prescribed){
 	        		comp_deliv_num = (exists(doc.other_treatment,"fluids") || check_drug_name(drugs_prescribed,"sodium_chloride")  || check_drug_name(drugs_prescribed,"ringers_lactate")) ? 1 : 0;
@@ -207,10 +203,6 @@ function(doc) {
 	        	comp_deliv_num = (doc.resolution == "referral" || doc.admitted == "y") ? 1 : 0;
 	        	mgmt_good_so_far = comp_deliv_num;
 	        }
-	        /*If made it this far, dont have delivery complications*/
-	        if (comp_deliv_denom == 0) {
-	        	comp_deliv_num = 0;
-	    	}
 	        _emit("del_mgmt", comp_deliv_num, comp_deliv_denom);
         }
 
@@ -218,16 +210,15 @@ function(doc) {
         #--------------------------------------------
         #15.  Drugs dispensed appropriately (combined with Delivery form and Sick ANC)
         */
+		var drug_stock_denom = 0;
+        var drug_stock_num = 0;
 		if (doc.drugs && doc.drugs.prescribed && doc.drugs.prescribed.med) {
 	       drug_stock_denom = 1;
 	       drug_stock_num = check_drug_stock(doc.drugs.prescribed.med);
-	    } else {
-	       drug_stock_denom = 0;
-	       drug_stock_num = 0;
-	    }
+	    } 
 	   	_emit("drugs_stocked", drug_stock_num, drug_stock_denom);
         
-    } else if (doc["doc_type"] == "PregnancyReportRecord") {
+    } else if (doc.doc_type == "PregnancyReportRecord") {
 
         // this is where the aggregated data across pregnancies goes.
         
@@ -236,7 +227,7 @@ function(doc) {
             emit([date.getFullYear(), date.getMonth(), doc.clinic_id, name], [num, denom]);    
         }
     
-        first_visit_date = parse_date(doc.first_visit_date);
+        var first_visit_date = parse_date(doc.first_visit_date);
         /* this field keeps track of total pregnancies */
         _emit_with_custom_date(first_visit_date, "total_pregs",1,1);
         
