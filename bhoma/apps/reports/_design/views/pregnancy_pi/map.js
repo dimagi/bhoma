aa77afa434ec7292f6ab276edbd9bf9ea2090a90
Cory Zue <czue@dimagi.com>
2011-09-08 16:06:26 -0400

    fix haart PI denominator

--- bhoma/apps/reports/_design/views/pregnancy_pi/map.js
+++ bhoma/apps/reports/_design/views/pregnancy_pi/map.js
@@ -287,10 +287,12 @@ function(doc) {
 	    */
         
 		if (doc.had_two_healthy_visits_after_pos_test_ga_14 !== undefined) {
-		    _emit_with_custom_date(first_visit_date, "azt_haart", doc.got_azt_haart_on_consecutive_visits ? 1:0,
+		    _emit_with_custom_date(first_visit_date, "azt_haart", 
+		          (doc.got_azt_haart_on_consecutive_visits && doc.had_two_healthy_visits_after_pos_test_ga_14) ? 1:0,
                   doc.had_two_healthy_visits_after_pos_test_ga_14 ? 1:0);
 		} else {
-		    _emit_with_custom_date(first_visit_date, "azt_haart", doc.got_azt_haart_on_consecutive_visits ? 1:0,
+		    _emit_with_custom_date(first_visit_date, "azt_haart", 
+		          (doc.got_azt_haart_on_consecutive_visits && doc.ever_tested_positive) ? 1:0,
                   doc.ever_tested_positive ? 1:0);
 		}
 		
