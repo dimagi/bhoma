3713c325b2864c3433ad5acfb6a4cd7c47e6536f
Cory Zue <czue@dimagi.com>
2011-09-14 18:07:20 -0400

    reorder tests

--- bhoma/apps/case/tests/__init__.py
+++ bhoma/apps/case/tests/__init__.py
@@ -1,13 +1,13 @@
 try:
+    from bhoma.apps.case.tests.test_adult_visit import *
+    from bhoma.apps.case.tests.test_chw_referrals import *
+    from bhoma.apps.case.tests.test_clinic_cases import *
+    from bhoma.apps.case.tests.test_death import *
     from bhoma.apps.case.tests.test_from_xform import *
     from bhoma.apps.case.tests.test_in_patient import *
-    from bhoma.apps.case.tests.test_clinic_cases import *
+    from bhoma.apps.case.tests.test_ltfu import *
     from bhoma.apps.case.tests.test_phone_followups import *
     from bhoma.apps.case.tests.test_pregnancy import *
-    from bhoma.apps.case.tests.test_adult_visit import *
-    from bhoma.apps.case.tests.test_death import *
-    from bhoma.apps.case.tests.test_ltfu import *
-    from bhoma.apps.case.tests.test_chw_referrals import *
     from bhoma.apps.case.tests.test_xml import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
