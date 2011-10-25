afcaec5bfaf5233ee1950dc529f0716b53cdf484
Cory Zue <czue@dimagi.com>
2011-10-25 14:48:40 -0400

    DHMT support for couchlog (tag logs with a special code)

--- bhoma/apps/bhomalog/signals.py
+++ bhoma/apps/bhomalog/signals.py
@@ -3,7 +3,8 @@ from couchlog.signals import couchlog_created
 
 def add_bhoma_extras(record, **kwargs):
     # bolt on the clinic code and app version, as well as our extra_info
-    record.clinic_id = settings.BHOMA_CLINIC_ID
+    record.clinic_id = settings.BHOMA_CLINIC_ID if not settings.BHOMA_IS_DHMT \
+                       else "%sDHMT" % settings.BHOMA_CLINIC_ID
     record.commit_id = settings.BHOMA_COMMIT_ID
     # poor man's AppVersionedDoc
     record.app_version = settings.APP_VERSION
